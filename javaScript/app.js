2423898abbc2fb4f47f2a9474f8d400d4e1d8180
osama <>
2019-10-14 15:31:47 +0300

    web application compleation - need few adjustment for style

--- javaScript/app.js
+++ javaScript/app.js
@@ -1,101 +1,67 @@
-$("document").ready(function() {
-    
-    // declaring icons to choose from
-    const icons = [
-{
-    type:"X",
-    image:"../images/x.png"
-},
-{
-    type:"O",
-    image:"../images/o.png"
-},
-{
-    type:"Diamond",
-    image:"../images/cut-diamond_39242.png"
-},
-{
-    type:"Crown",
-    image:"../images/queen-crown_38957.png"
-},
-{
-    type:"Wolf",
-    image:"../images/wolf-head_38725.png"
-},
-{
-    type:"House",
-    image:"../images/house_38533.png"
-},
-{
-    type:"Ball",
-    image:"../images/soccer-ball_39433.png"
-},
-{
-    type:"Cat",
-    image:"../images/white-cat_38735.png"
+const box = document.querySelectorAll(".wh"); 
+let x_or_o = 0;
+const s1 = document.querySelector(".s1");
+const s2 = document.querySelector(".s2");
+const s3 = document.querySelector(".s3");
+const s4 = document.querySelector(".s4");
+const s5 = document.querySelector(".s5");
+const s6 = document.querySelector(".s6");
+const s7 = document.querySelector(".s7");
+const s8 = document.querySelector(".s8");
+const s9 = document.querySelector(".s9");
+
+const replay = function(){
+    for (let i = 0; i < box.length; i++) {
+        box[i].classList.remove("winning");
+        box[i].innerHTML = "";
+    }
 }
-];
 
-// declaring the board and the boxes
+const winningRow = function(b1,b2,b3){
+    b1.classList.add("winning");
+    b2.classList.add("winning");
+    b3.classList.add("winning");
+}
 
-const $box = $('.wh');
-    const s1 = $('.s1');
-    const s2 = $('.s2');
-    const s3 = $('.s3');
-    const s4 = $('.s4');
-    const s5 = $('.s5');
-    const s6 = $('.s6');
-    const s7 = $('.s7');
-    const s8 = $('.s8');
-    const s9 = $('.s9');
+const winner = function(){
+    if(s1.innerHTML !== "" && s1.innerHTML === s2.innerHTML && s1.innerHTML === s3.innerHTML){
+        winningRow(s1, s2, s3);
+        }
+    if(s4.innerHTML !== "" && s4.innerHTML === s5.innerHTML && s4.innerHTML === s6.innerHTML){
+        winningRow(s4, s5, s6);
+        }
+    if(s7.innerHTML !== "" && s7.innerHTML === s8.innerHTML && s7.innerHTML === s9.innerHTML){
+        winningRow(s7, s8, s9);
+        }
+    if(s1.innerHTML !== "" && s1.innerHTML === s4.innerHTML && s1.innerHTML === s7.innerHTML){
+        winningRow(s1, s4, s7);
+        }
+    if(s2.innerHTML !== "" && s2.innerHTML === s5.innerHTML && s2.innerHTML === s8.innerHTML){
+        winningRow(s2, s5, s8);
+        }
+    if(s3.innerHTML !== "" && s3.innerHTML === s6.innerHTML && s3.innerHTML === s9.innerHTML){
+        winningRow(s3, s6, s9);
+        }
+    if(s1.innerHTML !== "" && s1.innerHTML === s5.innerHTML && s1.innerHTML === s9.innerHTML){
+        winningRow(s1, s5, s9);
+        }
+    if(s3.innerHTML !== "" && s3.innerHTML === s5.innerHTML && s3.innerHTML === s7.innerHTML){
+        winningRow(s3, s5, s7);
+        }
+}
 
-    // declaring the count  
-
-    const count = 0;
-
-    // declaring the winner
-
-    winner = "";
-
-    // showing which box has been choosen
-    
-    const onClickk = function(){
-        let a = $(this).attr("class")
-        console.log(this.id);
-        
-        const choose = function(shape){
-            for (let i = 0; i < icons.length; i++) {
-                if (icons[i].type === shape){
-                    return icons[i].image
-                }
-                
+for (let i = 0; i < box.length; i++) {
+    box[i].onclick = function(){
+        if(this.innerHTML !== '<img class="imge" src="../images/x.png">' && this.innerHTML !== '<img class="imge" src="../images/o.png">'){
+            if(x_or_o%2 === 0){
+                this.innerHTML = '<img class="imge" src="../images/x.png">';
+                winner();
+                x_or_o += 1;
+            } else {
+                this.innerHTML = '<img class="imge" src="../images/o.png">';
+                winner();
+                x_or_o += 1;
             }
         }
-        $(`#${this.id} > img`).attr("src", "../images/white-cat_38735.png")
-    }
-    $('.wh').click(onClickk);
-    
-    function checkForGame(){
-        function startGame ()
-           {
-               if (s1 === s2 === s3 || s1 === s4 === s7 || s1 === s5 === s9 || s2 === s5 === s8 || s3 === s6 === s9 || s3 === s5 === s7 || s4 === s5 === s6 || s7 === s8 === s9){
-                   alert('w');
-               } else { 
-                   alert('Q');
-               }
-           }
-       
-           if (icons.length === 2){
-       } else { 
-    
-       } } 
-       
-    
-    let s = choose('O');
-    console.log(s);
-
-// checking to see if the game has two players or not 
-    
-
-
-});
+    }   
+}
